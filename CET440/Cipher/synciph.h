8080d9000c75951d06b200a8c205a242abe63bdd
spate-ar <91082913+spate-ar@users.noreply.github.com>
2021-09-27 08:43:47 -0400

    Create synciph.h

---
+++ CET440/Cipher/synciph.h
@@ -0,0 +1,11 @@
+#ifndef SYNCIPH
+#define SYNCIPH
+#include <stddef.h>
+
+// Function Prototypes
+void createPChars(char* pChars);
+void printPChars(char* pChars);
+void createCipher(char* pChars, char* cipher);
+void shuffle(char* pChars, size_t n);
+
+#endif
